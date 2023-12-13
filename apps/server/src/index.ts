546634077900f922259ab14c7c85321cc36268bd
anav5704 <anavkki@gmail.com>
2023-12-14 11:48:17 +1200

    feat(api):  create http and ws servers

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world: Turbo repo")
+import http from "http"
+import { SocketService } from "./services/socket"
+
+(async () => {
+    const socketService = new SocketService()
+    const httpServer = http.createServer()
+    const PORT = process.env.PORT || 8000
+
+    socketService.io.attach(httpServer)
+    socketService.initListeners()
+
+    httpServer.listen(PORT, () => {
+        console.log("Http server started at port:", PORT)
+    })
+})()
+
---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,28 @@
+import { Server, Socket } from "socket.io"
+
+export class SocketService {
+    private _io: Server
+
+    constructor(){
+        console.log("Web socket server started")
+        this._io = new Server()
+    }
+
+    get io(){
+        return this._io
+    }
+
+    public initListeners(){
+        const io = this._io
+        console.log("Initialsied web socket event listeners")
+
+        io.on("connect", (socket) => {
+            console.log("New web socket connectin established", socket.id)
+        })
+
+        io.on("event:message", async ({ message }: { message: string }) => {
+            console.log("New message recieved", message)
+        })
+
+    }
+}
