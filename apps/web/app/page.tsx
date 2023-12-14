dd893077460ee0dfa43bffa82dbfc7691977a24d
anav5704 <anavkki@gmail.com>
2023-12-14 18:39:01 +1200

    feat: add username form

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,20 +3,32 @@
 import { useSocket } from "./context/SocketProvider"
 import { useState } from "react"
 
-export default function HomePage(){
+export default function HomePage() {
     const { sendMessage, messages } = useSocket()
-
+    const [username, setUsername] = useState("")
+    const [user, setUser] = useState(false)
     const [message, setMessage] = useState<string>("")
 
     return (
         <main>
-            <div>
-                {messages.map((message, index) => (
-                    <p key={index}>{message}</p>
-                ))}
-            </div>
-            <input value={message} onChange={(e) => setMessage(e.target.value)} type="text" name="" id="" />
-            <button onClick={() => sendMessage(message)}>Send</button>
+            { !user ? (
+                <div>
+                    <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" type="text" name="" id="" />
+                    <button onClick={() => setUser(true)}>Continue</button>
+                </div>
+            ) : (
+                <>
+                    <div>
+                        {messages.map((message, index) => (
+                            <p key={index}>{message}</p>
+                        ))}
+                    </div>
+                    {username}
+                    <input value={message} onChange={(e) => setMessage(e.target.value)} type="text" name="" id="" />
+                    <button onClick={() => sendMessage(message)}>Send</button>
+
+                </>
+            )}
         </main>
     )
 }
