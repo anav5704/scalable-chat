30e27299a53bd3390df9e909a3aa82884dc41405
anav5704 <anavkki@gmail.com>
2023-12-14 15:07:32 +1200

    feat: add skeleton chat ui

--- apps/web/app/context/SocketProvider.tsx
+++ apps/web/app/context/SocketProvider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { createContext, useCallback, useEffect } from "react"
-import { io } from "socket.io-client"
+import { createContext, useCallback, useContext, useEffect, useState } from "react"
+import { Socket, io } from "socket.io-client"
 
 interface SocketProviderProps {
     children: React.ReactNode
@@ -13,16 +13,29 @@ interface ISocketContext {
 
 const SocketContext = createContext<ISocketContext | null>(null)
 
+export const useSocket = () => {
+    const state = useContext(SocketContext)
+    if (!state) throw new Error("State is un defined")
+    return state
+}
+
 export const SocketProvider = ({ children }: SocketProviderProps) => {
+    const [socket, setSocket] = useState<Socket>()
+
     const sendMessage: ISocketContext["sendMessage"] = useCallback((message) => {
         console.log(message)
-    }, [])
+        if(socket){
+            socket.emit("event:message", { message })
+        }
+    }, [socket])
 
     useEffect(() => {
         const _socket = io("http://localhost:8000")
+        setSocket(_socket)
 
         return () => {
             _socket.disconnect()
+            setSocket(undefined)
         }
     }, [])
 
