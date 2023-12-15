fbf6aa91b6890d6549101bc7cd7a58c54ceaa0b5
anav5704 <anavkki@gmail.com>
2023-12-15 13:59:14 +1200

    chore: change deployed ws url

--- apps/web/app/context/SocketProvider.tsx
+++ apps/web/app/context/SocketProvider.tsx
@@ -36,7 +36,7 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
     }, [])
 
     useEffect(() => {
-        const _socket = io("http://localhost:8000")
+        const _socket = io("https://scaleable-chat.onrender.com")
         _socket.on("message", onMessgeRecieved)
         setSocket(_socket)
 
