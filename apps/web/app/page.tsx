8fd72e82407e1fe5dd5a0dc53c46a3d7a45e5896
anav5704 <anavkki@gmail.com>
2023-12-15 14:05:34 +1200

    style: add stlying to everything

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,28 +9,44 @@ export default function HomePage() {
     const [user, setUser] = useState(false)
     const [message, setMessage] = useState<string>("")
 
+    const handleSend = (e: any) => {
+        if(e.key === "Enter"){
+            e.preventDefault()
+            sendMessage({ message, username })
+            setMessage("")
+        }
+    }
+
     return (
         <main>
+            <div className="aurora">
+                <div className="canvas-wrapper">
+                    <div className='circle circle-0' />
+                    <div className='circle circle-1' />
+                    <div className='circle circle-2' />
+                    <div className='circle circle-3' />
+                </div>
+            </div>
             { !user ? (
-                <div>
+                <div className="box">
                     <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" type="text" name="" id="" />
                     <button onClick={() => setUser(true)}>Continue</button>
                 </div>
             ) : (
-                <>
-                    <div>
+                <div className="box3">
+                    <div className="msgbox">
                         {messages.map((msg, index) => (
-                            <div key={index}>
-                            <p>{msg.username}</p>
-                            <p>{msg.message}</p>
+                        <div key={index} className={`${msg.username === username && "right"} msg`}>
+                            <p className="name">{msg.username}</p>
+                            <p className="cont">{msg.message}</p>
                             </div>
                         ))}
                     </div>
-                    {username}
-                    <input value={message} onChange={(e) => setMessage(e.target.value)} type="text" name="" id="" />
-                    <button onClick={() => sendMessage({ message, username })}>Send</button>
+                    <div className="box box2">
+                        <input onKeyDown    ={(e) => handleSend(e)} placeholder="Enter message" value={message} onChange={(e) => setMessage(e.target.value)} type="text" name="" id="" />
+                    </div>
 
-                </>
+                </div>
             )}
         </main>
     )
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+         "../../packages/ui/**/*{.js,.ts,.jsx,.tsx}",
+    ],
+    theme: {
+        extend: {}, 
+    },
+    plugins: [],
 }
 
